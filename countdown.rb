968945ec8598e1a0f1fbbbd99dfd764ed17eb97f
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-02-10 13:36:49 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (integer)
+integer = 10
+while integer >= 1
+    puts "#{integer} SECOND(S)!"
+    integer -=1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep integer
+    integer = 10
+    while integer >= 1
+        sleep 0.5
+        puts "#{integer} SECOND(S)!"
+    integer -=1 
+    end
+    "HAPPY NEW YEAR!"
 end
